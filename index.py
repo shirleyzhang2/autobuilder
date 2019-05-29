7e9d7b5b314f8cd81a7589592b53fcb1bcbd9736
shirleyzhang2 <40873474+shirleyzhang2@users.noreply.github.com>
2019-05-28 23:51:05 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 import os
 import win32com.client
-import openpyxl
 import random
 from openpyxl import *
 
@@ -10,10 +9,19 @@ def get_excel_indices(ws, index_headings_col, index_values_col, index_start_row)
     while ws[index_headings_col + str(current_row)].value is not None:
         index_heading = ws[index_headings_col + str(current_row)].value
         index_value = ws[index_values_col + str(current_row)].value
-        for i in range(len(index_value_array)):
-            index_value_array[i] = int(index_value_array[i])
-            index_value = index_value_array
-        #enter the new entry into the index
         excel_index[index_heading] = index_value
         current_row = current_row + 1
     return excel_index
+
+wb = load_workbook('SetupAB.xlsx')
+ws = wb.get_sheet_by_name('Index')
+ExcelIndex = Generate_Tower.get_excel_indices(ws, 'A', 'B', 2)
+
+InputTable = ExcelIndex['Input table sheet']
+FloorPlan = ExcelIndex['Floor plans sheet']
+SectionProperties = ExcelIndex['Section properties sheet']
+Bracing = ExcelIndex['Bracing sheet']
+FloorBracing = ExcelIndex['Floor bracing sheet']
+Materials = ExcelIndex['Materials sheet']
+InputTableOffset = ExcelIndex['Input table offset']
+PropertiesStartRow = ExcelIndex['Properties start row']
